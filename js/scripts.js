9aa5b171de05e5825ca33e4478c9e40836ca8a23
greentap <taphighscore@gmail.com>
2015-09-04 08:22:49 -0400

    initial

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+var mCheck = false; 
+var userEmail; 
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	mCheck = true; 
+
+	document.getElementById("contactNav").innerHTML = "";
+	document.getElementById("titleText").style.fontSize= "130px"; 
+	document.getElementById("mainCaption").style.fontSize= "45px"; 
+	document.getElementById("mainCaption").style.textAlign= "center"; 
+}
+
+function email(){
+	if(mCheck){
+		window.location="mailto:me@tash-had.com"; 
+	}
+	else{
+		window.prompt("Press Ctrl+C to Copy", "me@tash-had.com"); 
+	}
+
+}
+
+
+function emailFunction(){
+alertify
+  .defaultValue("")
+  .okBtn("Submit")
+  .cancelBtn("Cancel")
+  .prompt("The iOS release is right around the corner! If you could kindly put in your e-mail, I can let you know when it comes out. :)",
+    function (val, event) {
+
+      // The click event is in the event variable, so you can use it here.
+      event.preventDefault();
+
+      // The value entered is availble in the val variable.
+      storeEmail(val); 
+
+    }, function(event) {
+
+      // The click event is in the event variable, so you can use it here.
+      event.preventDefault();
+
+      alertify.error("You've clicked Cancel");
+
+    }
+  );  
+}
+
+function storeEmail(email){
+	var emailObject = Parse.Object.extend("Emails_iOS"); 
+	var emailObj = new emailObject(); 
+
+	emailObj.set("email", email); 
+	emailObj.save(null, {
+		success: function(emailObj) {
+			alertify.success("Thank you!");
+  },
+  error: function(emailObj, error) {
+  	      alertify.error("Uh oh! Something went wrong! You can see my contact info below!");
+  }
+});
+}
