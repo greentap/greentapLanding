e76688411cc0f1feb26247528afe5ff8bb37b90e
tash-had <nanylagoon@gmail.com>
2015-09-04 18:30:01 -0400

    Miscellaneous

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,57 @@
-var mCheck = false; 
-var userEmail; 
+var mCheck = false;
+var userEmail;
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-	mCheck = true; 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    mCheck = true;
 }
 
-function email(){
-	if(mCheck){
-		window.location="mailto:me@tash-had.com"; 
-	}
-	else{
-		window.prompt("Press Ctrl+C to Copy", "me@tash-had.com"); 
-	}
-
-}
-
-
-function emailFunction(){
-alertify
-  .defaultValue("")
-  .okBtn("Submit")
-  .cancelBtn("Cancel")
-  .prompt("The iOS release is right around the corner! If you put in your e-mail, I can let you know when it comes out. :)",
-    function (val, event) {
-
-      // The click event is in the event variable, so you can use it here.
-      event.preventDefault();
-
-      // The value entered is availble in the val variable.
-      storeEmail(val); 
-
-    }, function(event) {
-
-      // The click event is in the event variable, so you can use it here.
-      event.preventDefault();
-
-      alertify.error("You've clicked Cancel");
-
+function email() {
+    if (mCheck) {
+        window.location = "mailto:me@tash-had.com";
+    } else {
+        window.prompt("Press Ctrl+C to Copy", "me@tash-had.com");
     }
-  );  
+
 }
 
-function storeEmail(email){
-	var emailObject = Parse.Object.extend("Emails_iOS"); 
-	var emailObj = new emailObject(); 
 
-	emailObj.set("email", email); 
-	emailObj.save(null, {
-		success: function(emailObj) {
-			alertify.success("Thank you!");
-  },
-  error: function(emailObj, error) {
-  	      alertify.error("Uh oh! Something went wrong! You can see my contact info below!");
-  }
-});
+function emailFunction() {
+    alertify
+        .defaultValue("")
+        .okBtn("Submit")
+        .cancelBtn("Cancel")
+        .prompt("The iOS release is right around the corner! If you put in your e-mail, I can let you know when it comes out. :)",
+            function(val, event) {
+
+                // The click event is in the event variable, so you can use it here.
+                event.preventDefault();
+
+                // The value entered is availble in the val variable.
+                storeEmail(val);
+
+            },
+            function(event) {
+
+                // The click event is in the event variable, so you can use it here.
+                event.preventDefault();
+
+                alertify.error("You've clicked Cancel");
+
+            }
+        );
+}
+
+function storeEmail(email) {
+    var emailObject = Parse.Object.extend("Emails_iOS");
+    var emailObj = new emailObject();
+
+    emailObj.set("email", email);
+    emailObj.save(null, {
+        success: function(emailObj) {
+            alertify.success("Thank you!");
+        },
+        error: function(emailObj, error) {
+            alertify.error("Uh oh! Something went wrong! You can see my contact info below!");
+        }
+    });
 }
